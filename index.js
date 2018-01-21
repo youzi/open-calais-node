f58fdad3ddca9095bd77d5c29b101f8d1b032e75
Youri Daamen <youridaamen@gmail.com>
2018-01-21 19:17:13 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const textract = require('textract');
+const got = require('got');
+const isPlural = require('is-plural');
+const arg = process.argv[2];
+const url = 'https://api.thomsonreuters.com/permid/calais';
+const isUrl = require('is-url');
+
+const analyzeString = body => {
+  const headers = {
+    'x-ag-access-token': process.env.OPEN_CALAIS_TOKEN,
+    OutputFormat: 'application/json',
+  };
+  return got(url, {
+    headers,
+    body,
+  });
+};
+
+const parseResponse = response => {
+  const obj = JSON.parse(response.body);
+  const result = {};
+  Object.keys(obj).forEach(key => {
+    const {_typeGroup, _type, name} = obj[key];
+    if (name && key !== 'doc' && !/\:| - /.test(name) && _type !== 'Position') {
+      const question =
+        _type === 'Person'
+          ? `Who is ${name}?`
+          : isPlural(name) ? `What are ${name}?` : `What is ${name}?`;
+      result[question.toLowerCase()] = question;
+    }
+  });
+  Object.keys(result).forEach((key, index) => {
+    console.log(result[key]);
+  });
+};
+
+const getString = new Promise((resolve, reject) => {
+  const cb = (err, res) => (err ? reject(err) : resolve(res));
+  isUrl(arg) ? textract.fromUrl(arg, cb) : textract.fromFileWithPath(arg, cb);
+});
+
+getString
+  .then(analyzeString)
+  .then(parseResponse)
+  .catch(error => {
+    console.log(error);
+  });

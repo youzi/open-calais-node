03bbb931f37fd118be283130d19a02141cbbc61c
Youri Daamen <youridaamen@gmail.com>
2018-02-01 17:22:16 +0800

    setup

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const textract = require('textract');
-const got = require('got');
-const isPlural = require('is-plural');
+const textract = require("textract");
+const got = require("got");
+const isPlural = require("is-plural");
+
 const arg = process.argv[2];
-const url = 'https://api.thomsonreuters.com/permid/calais';
-const isUrl = require('is-url');
+const url = "https://api.thomsonreuters.com/permid/calais";
+const isUrl = require("is-url");
 
 const analyzeString = body => {
   const headers = {
-    'x-ag-access-token': process.env.OPEN_CALAIS_TOKEN,
-    OutputFormat: 'application/json',
+    "x-ag-access-token": process.env.OPEN_CALAIS_TOKEN,
+    OutputFormat: "application/json"
   };
   return got(url, {
     headers,
-    body,
+    body
   });
 };
 
@@ -20,33 +21,33 @@ const parseResponse = response => {
   const obj = JSON.parse(response.body);
   const result = {};
   Object.keys(obj).forEach(key => {
-    const {_typeGroup, _type, name} = obj[key];
-    if (name && key !== 'doc' && !/\:| - /.test(name) && _type !== 'Position') {
+    const { _type, name } = obj[key];
+    if (name && key !== "doc" && !/:| - /.test(name) && _type !== "Position") {
       const question =
-        _type === 'Person'
+        _type === "Person"
           ? `Who is ${name}?`
           : isPlural(name) ? `What are ${name}?` : `What is ${name}?`;
       result[question.toLowerCase()] = question;
     }
   });
-  Object.keys(result).forEach((key, index) => {
+  Object.keys(result).forEach(key => {
     console.log(result[key]);
   });
 };
 
-const getString = new Promise((resolve, reject) => {
+const getString = new Promise(resolve => {
   if (process.stdin.isTTY) {
     const cb = (err, res) => (err ? resolve(arg) : resolve(res));
     isUrl(arg) ? textract.fromUrl(arg, cb) : textract.fromFileWithPath(arg, cb);
   } else {
     const stdin = process.openStdin();
-    let data = '';
+    let data = "";
 
-    stdin.on('data', chunk => {
+    stdin.on("data", chunk => {
       data += chunk;
     });
 
-    stdin.on('end', () => {
+    stdin.on("end", () => {
       resolve(data);
     });
   }
